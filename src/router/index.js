01ea1e9be1045bef9389a66f4c10d357e1074e92
Kübra Bayazıt <kubra.bayazit@gricreative.com>
2021-08-31 17:41:09 +0300

    Typo

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Chat from '@/pages/chat'
-import Login from '@/pages/login'
+import Chat from '@/pages/Chat'
+import Login from '@/pages/Login'
 
 Vue.use(Router)
 
